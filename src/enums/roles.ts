7dff91b315b12859096328339f46500d2b2822d4
Andrew McCallum <amccallum89@gmail.com>
2022-10-13 19:52:43 +0100

    first commit

---
+++ src/enums/roles.ts
@@ -0,0 +1,7 @@
+export enum Roles {
+  'Admin' = 'Admin',
+  'Editor' = 'Editor',
+  'Author' = 'Author',
+  'Contributor' = 'Contributor',
+  'Subscriber' = 'Subscriber'
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './types/user'
+export * from './enums/roles'
